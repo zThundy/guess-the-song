17a6e50137811a58d9290897fc86034a2d0872b2
zThundy <antonioantonelli7@gmail.com>
2023-12-12 17:18:07 +0100

    Update prelobbyUser styles and add dragging functionality

--- src/components/maingame/prelobbygame/main.jsx
+++ src/components/maingame/prelobbygame/main.jsx
@@ -3,10 +3,38 @@ import "./main.css";
 import { useRef, useEffect, useState } from 'react';
 
 import { motion } from 'framer-motion';
-import { Typography } from "@mui/material";
+import { Typography, Badge } from "@mui/material";
+import { Person } from "@mui/icons-material";
 
 function Users({ customRef }) {
-  const [users, setUsers] = useState([{ self: true, name: 'You' }]);
+  const [users, setUsers] = useState([
+    { self: true, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' },
+    { self: false, name: 'You' }
+  ]);
+
+  const [dragDirection, setDragDirection] = useState({ x: 0, y: 0, angle: 0 });
+
+  useEffect(() => {
+    console.log("dragDirection", dragDirection)
+  }, [dragDirection])
 
   return (
     <>
@@ -15,25 +43,47 @@ function Users({ customRef }) {
           return (
             <motion.div
               key={i}
-              className="prelobbyUser"
+              id={"prelobbyUser-" + i}
+              className="prelobbyUser grab"
               drag
               dragConstraints={customRef}
               dragSnapToOrigin={false}
+              dragPropagation={false}
               dragElastic={0.1}
               whileDrag={{
                 scale: 1.2,
-                backgroundColor: user.self ? 'rgba(0, 255, 0, 1)' : 'rgba(255, 0, 0, 1)',
+                backgroundColor: user.self ? 'rgba(204,102,0,1)' : 'rgba(255,171,43,1)',
               }}
-              onMouseDown={(e) => {
-                e.target.style.cursor = 'grabbing';
-                e.target.style.zIndex = 100;
+              onDragStart={(e) => {
+                if (!user.angle) user.angle = 0;
+                const target = document.getElementById("prelobbyUser-" + i);
+                target.classList.add("grabbing");
+                target.classList.remove("grab");
               }}
-              onMouseUp={(e) => {
-                e.target.style.cursor = 'grab';
-                e.target.style.zIndex = 0;
+              onDrag={(e) => {
+                if (!user.angle) user.angle = 0;
+                // get direction of drag
+                const newX = e.clientX;
+                const newY = e.clientY;
+                if (newX !== dragDirection.x || newY !== dragDirection.y)
+                  setDragDirection((prev) => {
+                    // Calculate the angle in radians
+                    const angle = Math.atan2(newY - prev.y, newX - prev.x);
+                    const angleInDegrees = (angle * (180 / Math.PI) + 360) % 360;
+                    // check if the angle is not too different from the previous angle
+                    if (Math.abs(angleInDegrees - prev.angle) > 10) {
+                      user.angle = angleInDegrees;
+                      return { x: newX, y: newY, angle: angleInDegrees };
+                    }
+                    else
+                      return prev;
+                  });
               }}
-              onMouseEnter={(e) => {
-                e.target.style.cursor = 'grab';
+              onDragEnd={(e) => {
+                if (!user.angle) user.angle = 0;
+                const target = document.getElementById("prelobbyUser-" + i);
+                target.classList.remove("grabbing");
+                target.classList.add("grab");
               }}
               initial={{ scale: 0 }}
               animate={{ scale: [1, 1.1, 1] }}
@@ -43,7 +93,20 @@ function Users({ customRef }) {
                 left: ((i * 3) + 4) + 'rem',
               }}
             >
-              {i}
+              <Badge
+                invisible={!user.self}
+                badgeContent="You"
+                color="primary"
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                sx={{
+                  transform: `rotate(${(user.angle + 90)}deg)`,
+                }}
+              >
+                <Person />
+              </Badge>
             </motion.div>
           )
         })
@@ -54,14 +117,19 @@ function Users({ customRef }) {
 
 function PrelobbyGame() {
   const constraintsRef = useRef(null);
+  const [shouldRender, setShouldRender] = useState(false);
   const center = constraintsRef.current?.getBoundingClientRect();
-  const [ready, setReady] = useState(false);
 
   useEffect(() => {
-    setReady(false);
-    if (constraintsRef.current) {
-      // this forces the element to rerender because of the new value
-      setReady(true);
+    constraintsRef.current = document.querySelector('.prelobbyContent');
+    console.log(center);
+    if (center && (center.width !== 0 || center.height !== 0)) {
+      setShouldRender(true);
+    } else {
+      setShouldRender(true);
+      setTimeout(() => {
+        setShouldRender(false);
+      }, 200);
     }
   }, [constraintsRef, center]);
 
@@ -77,9 +145,7 @@ function PrelobbyGame() {
         <div className="prelobbyContentOuter">
           <motion.div className='prelobbyContent' ref={constraintsRef}>
             <Typography variant='h6' className='prelobbyTitle'>Toss users around 🙃</Typography>
-            {
-              ready ? <Users customRef={constraintsRef} /> : null
-            }
+            {shouldRender ? <Users customRef={constraintsRef} /> : null}
           </motion.div>
         </div>
       </div>
