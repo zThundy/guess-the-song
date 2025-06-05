7d64881718e716f81e175b2654604a51bfcd55b0
zThundy <antonioantonelli7@gmail.com>
2025-06-05 16:05:21 +0200

    removed fake timeout from create route

--- server/src/routes/create.ts
+++ server/src/routes/create.ts
@@ -11,81 +11,77 @@ createRouter.use((req: Request, res: Response, next) => {
 createRouter.get("/genres", (req: Request, res: Response) => {
   // res.status(404).json({ key: "CREATE_ERROR_LOAD_GENRES", message: "Genres not found" })
 
-  setTimeout(() => {
-    res.json([
-      {
-        name: "Rock",
-      },
-      {
-        name: "New",
-        icon: "AddCircle"
-      },
-      {
-        name: "Sanremo",
-        icon: "LibraryMusic"
-      },
-      {
-        name: "Pop",
-      },
-      {
-        name: "Jazz",
-      },
-      {
-        name: "Classical",
-      },
-      {
-        name: "Hip Hop",
-      },
-      {
-        name: "Rap",
-        icon: "MusicNote"
-      },
-      {
-        name: "Country",
-        icon: "Movie"
-      },
-      {
-        name: "Metal",
-      },
-      {
-        name: "Japanese",
-      },
-      {
-        name: "Korean",
-        icon: "SportsEsports"
-      },
-    ])
-  }, 5000)
+  res.json([
+    {
+      name: "Rock",
+    },
+    {
+      name: "New",
+      icon: "AddCircle"
+    },
+    {
+      name: "Sanremo",
+      icon: "LibraryMusic"
+    },
+    {
+      name: "Pop",
+    },
+    {
+      name: "Jazz",
+    },
+    {
+      name: "Classical",
+    },
+    {
+      name: "Hip Hop",
+    },
+    {
+      name: "Rap",
+      icon: "MusicNote"
+    },
+    {
+      name: "Country",
+      icon: "Movie"
+    },
+    {
+      name: "Metal",
+    },
+    {
+      name: "Japanese",
+    },
+    {
+      name: "Korean",
+      icon: "SportsEsports"
+    },
+  ])
 })
 
 createRouter.get("/categories", (req: Request, res: Response) => {
   // res.status(404).json({ key: "CREATE_ERROR_LOAD_CATEGORIES", message: "Categories not found" })
 
-  setTimeout(() => {
-    res.json([
-      {
-        name: "Music",
-        icon: "MusicNote"
-      },
-      {
-        name: "Movies",
-        icon: "Videocam"
-      },
-      {
-        name: "TV Shows",
-        icon: "Movie"
-      },
-      {
-        name: "Video Games",
-        icon: "SportsEsports"
-      },
-      {
-        name: "Coming soon...",
-        icon: "HourglassBottom",
-        disabled: true
-      }
-    ])
-  }, 5000)
+  res.json([
+    {
+      name: "Music",
+      icon: "MusicNote"
+    },
+    {
+      name: "Movies",
+      icon: "Videocam"
+    },
+    {
+      name: "TV Shows",
+      icon: "Movie"
+    },
+    {
+      name: "Video Games",
+      icon: "SportsEsports"
+    },
+    {
+      name: "Coming soon...",
+      icon: "HourglassBottom",
+      disabled: true
+    }
+  ])
 })
 
 export default createRouter;
