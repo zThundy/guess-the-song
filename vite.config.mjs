19bc229f80cd265a17d1aed9fd21312c3d5498e9
zThundy <antonioantonelli7@gmail.com>
2026-05-12 10:54:17 +0200

    added language as alias

--- vite.config.mjs
+++ vite.config.mjs
@@ -33,6 +33,7 @@ export default defineConfig({
         alias: {
             helpers: path.resolve(__dirname, './src/helpers'),
             components: path.resolve(__dirname, './src/components'),
+            language: path.resolve(__dirname, './src/language'),
             public: path.resolve(__dirname, './public'),
         },
     },
