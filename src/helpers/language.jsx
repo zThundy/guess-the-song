81218d40380593c5819be06ae00cdaff38871fe5
zThundy <antonioantonelli7@gmail.com>
2026-05-16 11:52:05 +0200

    added languages

--- src/helpers/language.jsx
+++ src/helpers/language.jsx
@@ -3,9 +3,14 @@
 const languages = [
     { value: "en", text: "English", flagCode: "gb" },
     { value: "it", text: "Italiano", flagCode: "it" },
-    // { value: "de", text: "Deutsch", flagCode: "de" },
-    // { value: "fr", text: "Français", flagCode: "fr" },
-    // { value: "es", text: "Español", flagCode: "es" },
+    { value: "de", text: "Deutsch", flagCode: "de" },
+    { value: "fr", text: "Français", flagCode: "fr" },
+    { value: "es", text: "Español", flagCode: "es" },
+    { value: "pt", text: "Português", flagCode: "pt" },
+    { value: "ja", text: "日本語", flagCode: "jp" },
+    { value: "zh", text: "中文", flagCode: "cn" },
+    { value: "sv", text: "Svenska", flagCode: "se" },
+    { value: "ko", text: "한국어", flagCode: "kr" },
 ];
 
 export function getAllowedLanguagesDetails() {
